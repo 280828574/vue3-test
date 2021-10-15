db68f02b8f42b865efdbcaa8a19840df816302b5
hanleng <hanleng@ruikun.com>
2021-10-15 15:45:32 +0800

    实验

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,37 @@
 <template>
-  <div id="about">2222</div>
+  <div id="about">{{ msg }}</div>
+  <el-button class="btn1" type="primary" plain @click="menuClick">点击</el-button>
+  <div class="div">{{ height }}</div>
 </template>
 <script>
-  import { defineComponent } from 'vue';
+  import { defineComponent, ref } from 'vue';
 
   export default defineComponent({
-    setup() {},
+    props: {
+      msg: {
+        // 菜单数据
+        type: String,
+        required: true,
+        default() {
+          return '222';
+        },
+      },
+    },
+    setup(props, { emit }) {
+      function menuClick() {
+        height.value = '333px';
+        // 通过自定义事件回传值
+        emit('changeMsg', height);
+      }
+      let height = ref('33px');
+      return { menuClick, height };
+    },
   });
 </script>
+<style>
+  .div {
+    width: 100px;
+    background: red;
+    height: v-bind(height);
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <p>{{ x }} {{ y }}</p>
   <el-button @click="change">更改</el-button>
+  <div>about</div>
+  <about @changeMsg="changeMsg" msg="333"></about>
 </template>
 
 <script>
-  import api from '../api';
+  import api from '@/api';
+  import about from './About';
   import { ref, toRef, toRefs, reactive, onMounted } from 'vue';
   export default {
+    components: { about },
     setup() {
       //可以在不失去响应性的情况下破坏结构
       let a = {
@@ -16,7 +20,11 @@
           state.x++;
           state.y++;
         },
+        changeMsg: val => {
+          console.log('val2 :>> ', val);
+        },
       };
+
       onMounted(() => {
         api.user.fetchUsers().then(res => {
           console.log('res2222 :>> ', res);
